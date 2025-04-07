efd7287fa95b5436ef7ff02f617d4f27cf203cf1
mantra <91632003+mantra0519@users.noreply.github.com>
2025-04-07 18:36:09 -0230

    hyper parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -29,6 +29,7 @@ class ModelTrainer:
 
 
     def initiate_model_trainer(self,train_array,test_array):
+
         try:
             logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
@@ -112,8 +113,6 @@ class ModelTrainer:
             return r2_square
             
 
-
-
-            
+   
         except Exception as e:
             raise CustomException(e,sys)
