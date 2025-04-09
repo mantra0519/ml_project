a6c443a686be015331fac5b54576b2c1807a778d
mantra <91632003+mantra0519@users.noreply.github.com>
2025-04-09 16:37:21 -0230

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
+import os
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
@@ -42,4 +43,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    app.run(host="0.0.0.0",  port=int(os.environ.get('PORT', 5000)))        
